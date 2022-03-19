22f7a4a0cfddb4e84c6111fae6dfec245a751928
dharshini-anand <hellodharshu@live.com>
2022-03-19 08:21:54 -0700

    added user registration

--- components/coursedash.js
+++ components/coursedash.js
@@ -6,15 +6,19 @@ import { useState, useEffect } from "react";
 export default function CourseDash ( { course } ) {
     const auth = useAuth();
     const [score, setScore] = useState([])
-    const handleSubmit = async (e, score) => {
+    const [notes, setNotes] = useState([])
+    const handleSubmit = async (e, score, notes) => {
         e.preventDefault();
         const classDocRef = doc(db, `users/${auth.user?.uid}/classes`, course.id)
         try {
             await updateDoc(classDocRef, {
-                scores: arrayUnion(score)
+                scores: arrayUnion({
+                    score: score,
+                    notes: notes
+                })
             })
         } catch (err) {
-            alert(err)
+            alert (err)
         }
     }
     return (
@@ -23,15 +27,15 @@ export default function CourseDash ( { course } ) {
             <h3>Your goal score: {course.goal_score}</h3>
             <div>
                 <h3>Your previous scores:</h3>
-                {course.scores.map((score) => (
+                {course.scores && course.scores.map((score) => (
                 <p key={score}>{score}</p>
                 ))}
             </div>
             <div className="flex justify-center items-center flex-col">
                 <h4>Add a score:</h4>
-                <label htmlFor="score" className="block text-sm font-medium leading-5 text-gray-700">Add Score</label>
-                <input className= "appearance-none block w-1/6 object-center px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type='text' name='score' onChange={(e) => setScore(e.target.value)}/>
-                <button className= "w-f1/6 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"onClick={(e) => handleSubmit(e, score)}>Submit</button>
+                <input className= "appearance-none block w-1/6 object-center px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type='text' name='score' placeholder='New Score' onChange={(e) => setScore(e.target.value)}/>
+                <input className= "appearance-none block w-1/6 object-center px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" type='text' name='notes' placeholder="notes" onChange={(e) => setNotes(e.target.value)}/>
+                <button className= "w-f1/6 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"onClick={(e) => handleSubmit(e, score, notes)}>Submit</button>
             </div>
             
         </div>
--- pages/courses/[id].js
+++ pages/courses/[id].js
@@ -10,7 +10,8 @@ import {
   onSnapshot,
   collection,
   query,
-  where
+  where,
+  addDoc
 } from "firebase/firestore";
 import { db } from "../../lib/firebase"
 
@@ -20,6 +21,20 @@ export default function ProductPage () {
     const { cid } = router.query
     const [ course, setCourse ] = useState([])
     const [ registered, setRegistered ] = useState([])
+    const [ score, setScore ] = useState([])
+    const handleSubmit = async (e, score) => {
+      e.preventDefault()
+      const userClasses = collection(db, `users/${auth.user.uid}/classes`)
+      try {
+        await addDoc(userClasses, {
+          goal_score: score,
+          id: course.id,
+          name: course.name
+        })
+      } catch (err) {
+        alert(err)
+      }
+    }
     //useEffect
     // find current course in courses
     // find courses in user
@@ -32,8 +47,7 @@ export default function ProductPage () {
         const courseDoc = snapshot.docs.map(doc => {
           return {id: doc.id,...doc.data()}
         })
-        console.log(courseDoc[0])
-        setCourse(courseDoc[0])
+        setCourse(courseDoc.find(course => course.id === last))
       })
       let userCourseCall
       if (auth.user) {
@@ -42,7 +56,9 @@ export default function ProductPage () {
           const userCourse = snapshot.docs.map(doc => {
             return {id: doc.id,...doc.data()}
           })
-          if (userCourse.length == 1) {
+          const matchCourse = userCourse.filter(userCourse => userCourse.id === course.id)
+          console.log(matchCourse)
+          if (matchCourse.length == 1) {
             setRegistered(true)
           } else {
             setRegistered(false)
@@ -67,8 +83,18 @@ export default function ProductPage () {
   
         <div className={ style.header }>
           <h2>{ course.name }</h2>
-          <p>{ course.name }</p>
-          {(auth.user && registered) && (<p>Registered!</p>)}
+          <p>{ course.teacher }</p>
+          {registered == true
+          ? (<p>Registered!</p>) 
+          : (
+            <div className="flex justify-center items-center flex-col">
+              <h4>Register Here:</h4>
+              <label htmlFor="score" className="block text-sm font-medium leading-5 text-gray-700">Goal Score</label>
+              <input className= "appearance-none block w-1/6 object-center px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" placeholder= 'Goal Score' type='text' name='score' onChange={(e) => setScore(e.target.value)}/>
+              <button className= "w-f1/6 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"onClick={(e) => handleSubmit(e, score)}>Register</button>
+            </div>
+          )
+          }
         </div>
       </div>
     </>
